f3fc382ac2af55d561916c108e573de05abf2ebf
= <andersclement96@gmail.com>
2020-09-23 13:15:32 +0200

    fixed launch file

--- src/segwayrmp2/launch/bringup.py
+++ src/segwayrmp2/launch/bringup.py
@@ -1,38 +1,27 @@
+import pathlib
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import pathlib
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='segwayrmp2',
-            namespace='segwayrmp1',
-            executable='segway_node',
-            name='segwayrmp_driver',
-            parameters=[
-		    {'interface_type': 'usb'},
-		    {"serial_port": "/dev/ttyUSB0"},
-		    {'usb_selector': "index"},
-		    {'usb_index': 0},
-		    {'usb_serial_number': "00000000"},
-		    {'usb_description': "Robotic Mobile Platform"},
-		    {'motor_timeout': 0.5},
-		    {'frame_id': "base_link"},
-		    {'odom_frame_id': "odom"},
-		    {'invert_linear_vel_cmds': 'false'},
-		    {'invert_angular_vel_cmds': 'false'},
-		    {'broadcast_tf': 'true'},
-		    {'rmp_type': "200/400"},
-		    {'linear_pos_accel_limit': 0.0},
-		    {'linear_neg_accel_limit': 0.0},
-		    {'angular_pos_accel_limit': 0.0},
-		    {'angular_neg_accel_limit': 0.0},
-		    {'max_linear_vel': 0.0},
-		    {'max_angular_vel': 0.},
-		    {'linear_odom_scale': 1.0},
-		    {'angular_odom_scale': 1.0},
-		    {'reset_odometry': 'false'},
-		    {'odometry_reset_duration': 1.0}
-            ]
-        )
-    ])
+
+	ld = LaunchDescription()
+
+	config = os.path.join(
+		get_package_share_directory('segwayrmp2'),
+		'config',
+		'params.yaml'
+		)
+
+
+	node=Node(
+		package='segwayrmp2',
+		namespace='segwayrmp1',
+		executable='segway_node',
+		name='segwayrmp_driver',
+		parameters=[config]
+	)
+
+	ld.add_action(node)
+	return ld
