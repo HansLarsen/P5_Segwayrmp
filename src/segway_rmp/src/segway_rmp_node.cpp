71b82d29151b18a3756a8575e33a76122efc17f9
Anders Clement <andersclement96@gmail.com>
2020-11-02 15:41:47 +0100

    added pitch to base_link transform

--- src/segway_rmp/src/segway_rmp_node.cpp
+++ src/segway_rmp/src/segway_rmp_node.cpp
@@ -297,7 +297,8 @@ public:
         
         // Create a Quaternion from the yaw displacement
         geometry_msgs::Quaternion quat = 
-            tf::createQuaternionMsgFromYaw(yaw_displacement);
+            tf::createQuaternionMsgFromRollPitchYaw(0,this->sss_msg.segway.pitch_angle, yaw_displacement);
+            //tf::createQuaternionMsgFromYaw(yaw_displacement);
         
         // Publish the Transform odom->base_link
         if (this->broadcast_tf) {
