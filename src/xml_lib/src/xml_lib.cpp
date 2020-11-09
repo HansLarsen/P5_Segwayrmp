c38c36e512753c7561a47dea84d33a1726bd6b9c
Anders Clement <andersclement96@gmail.com>
2020-11-09 14:19:09 +0100

    changed namespace

--- src/xml_lib/src/xml_lib.cpp
+++ src/xml_lib/src/xml_lib.cpp
@@ -130,7 +130,7 @@ static const unsigned char TIXML_UTF_LEAD_0 = 0xefU;
 static const unsigned char TIXML_UTF_LEAD_1 = 0xbbU;
 static const unsigned char TIXML_UTF_LEAD_2 = 0xbfU;
 
-namespace tinyxml2
+namespace xml_lib
 {
 
 struct Entity {
@@ -2948,4 +2948,4 @@ bool XMLPrinter::Visit( const XMLUnknown& unknown )
     return true;
 }
 
-}   // namespace tinyxml2
+}   // namespace xml_lib
