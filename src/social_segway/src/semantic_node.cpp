be1ca1155e99d0bdf30ec0d1f2d14ddbdd3b816e
Ros <jan18@student.aau.dk>
2020-11-10 11:10:20 +0100

    added destructor and some cleanup

--- src/social_segway/src/semantic_node.cpp
+++ src/social_segway/src/semantic_node.cpp
@@ -6,6 +6,8 @@
 #include "geometry_msgs/Transform.h"
 #include <vector>
 #include <string>
+#include <signal.h>
+#include <cmath>
 
 using namespace xml_lib;
 
@@ -474,6 +476,9 @@ class Semantic_node
 
     float allowedDeviation = 5; // idk just some value for now
     float allowedDeviation2;
+    double d[];
+    double distance;
+    float x1, y1, z1, x2, y2, z2;
 
     ros::Subscriber detectionSub;
     ros::Timer timer;
@@ -481,36 +486,26 @@ class Semantic_node
 
     void detectionCallback(const social_segway::ObjectList &data)
     {
-
         for (auto detectedObject : data.objects){
             auto allObjects = map->getAllObjects();
+            x1 = detectedObject.transform.translation.x;
+            y1 = detectedObject.transform.translation.y;
+            z1 = detectedObject.transform.translation.z;
+
             for (auto object : allObjects){ 
+                x2 = object.transform.translation.x;
+                y2 = object.transform.translation.y;
+                z2 = object.transform.translation.z;
+                
+                distance = std::hypot(std::hypot(x1-x2,y1-y2),z1-z2);
 
-                if (((detectedObject.transform.translation.x < object.transform.translation.x + allowedDeviation &&
-                      detectedObject.transform.translation.x > object.transform.translation.x - allowedDeviation) ||
-                     (detectedObject.transform.translation.y < object.transform.translation.y + allowedDeviation &&
-                      detectedObject.transform.translation.y > object.transform.translation.y - allowedDeviation) ||
-                     (detectedObject.transform.translation.z < object.transform.translation.z + allowedDeviation &&
-                      detectedObject.transform.translation.z > object.transform.translation.z - allowedDeviation)) &&
-                    (detectedObject.objectClass == object.objectClass &&
-                     detectedObject.type == object.type))
-                {
-
+                if (distance < allowedDeviation && detectedObject.objectClass == object.objectClass && detectedObject.type == object.type){
                     // Merge items: detectedObject and object (or ignore detectedObject?)
                     continue;
                     mergeObjects(detectedObject, object);
                 }
-                else
-                { // add item to list
-                    social_segway::Object newObject;
-                    newObject.id = detectedObject.id;
-                    newObject.transform.translation.x = detectedObject.transform.translation.x;
-                    newObject.transform.translation.y = detectedObject.transform.translation.y;
-                    newObject.transform.translation.z = detectedObject.transform.translation.z;
-                    newObject.type = detectedObject.type;
-                    newObject.objectClass = detectedObject.objectClass;
-
-                    map->addObjectByPosition(newObject);
+                else{ // add item to list
+                    map->addObjectByPosition(detectedObject);
                 }
             }        
         }    
@@ -522,21 +517,28 @@ class Semantic_node
         auto allLooseObjects = map->getLooseObjects();
 
         for (auto Furniture : allFurniture){
-            for (auto LooseObject: allLooseObjects){
+            x1 = Furniture.transform.translation.x;
+            y1 = Furniture.transform.translation.y;
+            z1 = Furniture.transform.translation.z;
 
-                if (LooseObject.transform.translation.z > Furniture.transform.translation.z){
+            for (auto LooseObject: allLooseObjects){
+                x2 = LooseObject.transform.translation.x;
+                y2 = LooseObject.transform.translation.y;
+                z2 = LooseObject.transform.translation.z;
+
+                distance = std::hypot(x1-x2,y1-y2);
+
+                if (z2 > z1){ // if higher
 
                     if (Furniture.objectClass == "dinner_table")
                         allowedDeviation2 = 1; // estimated radius of the Furniture upper surface
                     else if(Furniture.objectClass == "shelve")
                         allowedDeviation2 = 2;
                     else if(Furniture.objectClass == "coffee_table")
-                        allowedDeviation2 = 0.5;                    
+                        allowedDeviation2 = 0.5;
+                    //ect                    
 
-                    if ((LooseObject.transform.translation.x < Furniture.transform.translation.x + allowedDeviation2 &&
-                        LooseObject.transform.translation.x > Furniture.transform.translation.x - allowedDeviation2) ||
-                        (LooseObject.transform.translation.y < Furniture.transform.translation.y + allowedDeviation2 &&
-                        LooseObject.transform.translation.y > Furniture.transform.translation.y - allowedDeviation2)){
+                    if (distance < allowedDeviation2){
 
                         map->placeObjectOnFurniture(LooseObject, Furniture);
                     }
@@ -558,19 +560,33 @@ public:
         timer = nh->createTimer(ros::Duration(1.0), &Semantic_node::checkOnTopTimerCallback, this);
     }
 
+    ~Semantic_node(){
+        map->saveMap();
+    }
 
 };
 
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+    keepRunning = 0;
+}
 
 
 
 int main(int argc, char **argv)
 {
+    signal(SIGINT, intHandler);
     Semantic_data_holder *map = new Semantic_data_holder();
 
     ros::init(argc, argv, "semantic_node");
     ros::NodeHandle n;
-    //ros::spin();
+
+    while (keepRunning)
+    {
+        ros::spin();
+    }
 
     return 0;
 }
