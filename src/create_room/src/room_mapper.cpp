8b2224570d0dbf34b202ee5ae2012790a540d393
Ros <olauri18@student.aau.dk>
2020-11-11 10:20:34 -0800

    added lines and text for rviz

--- src/create_room/src/room_mapper.cpp
+++ src/create_room/src/room_mapper.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include "create_room/Rvizcommand.h"
 #include <fstream>
-#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/Marker.h> 
 
 
 class Room{
@@ -15,7 +15,6 @@ private:
     
     std::string type_;
 
-   
 public:
 
     float coords[2][2];
@@ -60,7 +59,6 @@ private:
     ros::Subscriber sub;
     ros::ServiceServer service;
     Room room;
-    
     visualization_msgs::Marker marker;
     uint32_t shape = visualization_msgs::Marker::SPHERE;
 
@@ -70,7 +68,6 @@ private:
     geometry_msgs::PointStamped lastCoordinate;
 
     std::vector<Room> rooms;
-
     std::string slet = "delete";
     std::string sletalt = "delete_all";
     std::string publish = "publish";
@@ -80,11 +77,10 @@ private:
 
 
 
-
 public:
 
     Markers(ros::NodeHandle *node){
-        marker_pub = node->advertise<visualization_msgs::Marker>("visualization_marker", 1 ,this);
+        marker_pub = node->advertise<visualization_msgs::Marker>("visualization_marker", 5 ,this);
         service = node->advertiseService("rvizcommand",&Markers::command, this);
         sub = node->subscribe("clicked_point", 1, &Markers::callback, this);
 
@@ -97,12 +93,26 @@ public:
         y = point->point.y;
    
         ROS_INFO("coordinates: x: %f, y: %f",x,y);
+        if(marker_pub.getNumSubscribers() < 1){
+            ROS_WARN("Add Rviz subscriber");
+        }
+        else{
         room.getCoords(x,y);
+        createMarker();
+        marker.pose.position.x = x;
+        marker.pose.position.y = y;
+        marker.id=room.counter;
+        marker.ns = room.outType();
+        marker_pub.publish(marker);
+        }
+    }
 
-        marker.header.frame_id = "map";
+    void createMarker(){
+        marker.points.clear();
+        marker.header.frame_id = "/map";
         marker.header.stamp = ros::Time::now();
         marker.ns ="test"; //room.outType();
-        marker.id = room.counter;
+        
         marker.type = shape;
         marker.action = visualization_msgs::Marker::ADD;
         marker.pose.position.x = x;
@@ -112,30 +122,33 @@ public:
         marker.pose.orientation.y = 0.0;
         marker.pose.orientation.z = 0.0;
         marker.pose.orientation.w = 1.0;
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
+        marker.scale.x = 0.3;
+        marker.scale.y = 0.3;
+        marker.scale.z = 0.3;
         marker.color.r = 0.0f;
         marker.color.g = 1.0f;
         marker.color.b = 0.0f;
         marker.color.a = 1.0;
         marker.lifetime = ros::Duration();
-
-
-        marker.pose.position.x = x;
-        marker.pose.position.y = y;
-        marker.ns = room.outType();
-        marker_pub.publish(marker);
-
     }
 
     bool command(create_room::Rvizcommand::Request  &req,
             create_room::Rvizcommand::Response &res){
       
-        if(req.a == slet){
+       if(req.a == slet){
+            
+            for(int i = 0; i<4; i++){
+
+                createMarker();
+                marker.ns = rooms[req.b].outType();
+                marker.action = visualization_msgs::Marker::DELETE;
+                marker.id = i;
+                marker_pub.publish(marker);
+            }
             rooms.erase(rooms.begin()+req.b);
-            res.response = "Room deleted";  
+            res.response = "Room deleted";
         }
+
         else if(req.a == sletalt){
             rooms.clear();
             res.response = "Deleted all rooms";
@@ -180,7 +193,57 @@ public:
 
         }
         else if(req.a == save){
+
+            float nx1,ny1,nx2,ny2;
+            nx1 = room.coords[0][0];
+            ny1 = room.coords[0][1];
+            nx2 = room.coords[1][0];
+            ny2 = room.coords[1][1];
+            float square[4][2] = {{nx1,ny1},{nx1,ny2},{nx2,ny2},{nx2,ny1}};
+        
+
+
+            createMarker();
+            marker.ns = room.outType();
+            marker.pose.position.x = 0;
+            marker.pose.position.y = 0;
+            marker.color.r = 1.0f;
+            marker.scale.x = 0.2;
+            marker.type = visualization_msgs::Marker::LINE_STRIP;
+            geometry_msgs::Point p;
+            p.z = 0;
+            marker.id = 2;
+            for(int i = 0; i<4; i++){
+            
+            
+            p.x = square[i][0];
+            p.y = square[i][1];
+          
+            marker.points.push_back (p);
+            }
+
+            p.x = square[0][0];
+            p.y = square[0][1];
+            marker.points.push_back(p);
+            marker_pub.publish(marker);
+
+            createMarker();
+            marker.ns = room.outType();
+            marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+            marker.text = room.outType();
+            marker.scale.x = 0.3;
+            marker.scale.y = 0.3;
+            marker.scale.z = 0.4;
+            marker.id = 3;
+            marker.pose.position.x = (nx1+nx2+0.000001)/2;
+            marker.pose.position.y = (ny1+ny2+0.000001)/2;
+            marker.pose.position.z = 0.3;
+            marker_pub.publish(marker);
+
+            room.getType("test");
+
             rooms.push_back(room);
+
             res.response = "Room saved";
         }
 
