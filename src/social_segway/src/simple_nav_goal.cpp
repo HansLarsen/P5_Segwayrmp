7804e6b872cf499bf9f76cf66d1ed6353390b12a
Ros <jan18@student.aau.dk>
2020-11-30 10:50:31 +0100

    Simple goal to points.

--- src/social_segway/src/simple_nav_goal.cpp
+++ src/social_segway/src/simple_nav_goal.cpp
@@ -1,40 +1,66 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include "geometry_msgs/PointStamped.h"
+#include <vector>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+std::vector<geometry_msgs::PointStamped> my_points;
+
+void callback(const geometry_msgs::PointStamped::ConstPtr &point){
+    my_points.push_back(*point);
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "simple_navigation_goals");
+    ros::NodeHandle node;
 
-    //tell the action client that we want to spin a thread by default
-    MoveBaseClient ac("move_base", true);
+    ros::Subscriber sub = node.subscribe("clicked_point", 1, callback);
+    int working_index = 0;
 
-    //wait for the action server to come up
-    while (!ac.waitForServer(ros::Duration(5.0)))
-    {
-        ROS_INFO("Waiting for the move_base action server to come up");
+    while (ros::ok()) {
+        if (my_points.size() < 4) {
+            continue;
+            ros::Duration(1.0).sleep();
+        }
+
+        //tell the action client that we want to spin a thread by default
+        MoveBaseClient ac("move_base", true);
+
+        //wait for the action server to come up
+        while (!ac.waitForServer(ros::Duration(5.0)))
+        {
+            ROS_INFO("Waiting for the move_base action server to come up");
+        }
+
+        move_base_msgs::MoveBaseGoal goal;
+
+        //we'll send a goal to the robot to move 1 meter forward
+        goal.target_pose.header.frame_id = "map";
+        goal.target_pose.header.stamp = ros::Time::now();
+
+        goal.target_pose.pose.position.x = my_points[working_index].point.x;
+        goal.target_pose.pose.position.y = my_points[working_index].point.y;
+        goal.target_pose.pose.position.z = 0;
+        goal.target_pose.pose.orientation.w = 1.0;
+
+        ROS_INFO("Sending goal");
+        ac.sendGoal(goal);
+
+        ac.waitForResult();
+
+        if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+            ROS_INFO("Hooray, the base moved 1 meter forward");
+        else
+            ROS_INFO("The base failed to move forward 1 meter for some reason");
+
+        working_index++;
+        if (working_index > 3) {
+            working_index = 0;
+        }
     }
-
-    move_base_msgs::MoveBaseGoal goal;
-
-    //we'll send a goal to the robot to move 1 meter forward
-    goal.target_pose.header.frame_id = "base_link";
-    goal.target_pose.header.stamp = ros::Time::now();
-
-    goal.target_pose.pose.position.x = 1.0;
-    goal.target_pose.pose.orientation.w = 1.0;
-
-    ROS_INFO("Sending goal");
-    ac.sendGoal(goal);
-
-    ac.waitForResult();
-
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        ROS_INFO("Hooray, the base moved 1 meter forward");
-    else
-        ROS_INFO("The base failed to move forward 1 meter for some reason");
 
     return 0;
 }
